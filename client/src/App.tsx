975e1ad8d29b49a7c47ec0733b956ef47ba973df
Naoya Ono <ono.naoyaa@gmail.com>
2020-03-25 18:47:17 +0800

    Setup socket.io client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, FormEvent } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3030');
 
 function App() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  useEffect(() => {
+    socket.on('message', (msg: string) => {
+      setMessages(prev => [...prev, msg]);
+    });
+  }, []);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    socket.emit('newMessage', newMessage);
+    setNewMessage('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {messages.map((msg, i) => (
+          <div key={i}>{msg}</div>
+        ))}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={e => setNewMessage(e.target.value)} value={newMessage} />
+        <button type="submit" disabled={!newMessage}>Send</button>
+      </form>
     </div>
   );
 }
